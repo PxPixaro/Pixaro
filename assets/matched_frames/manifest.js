23fffe997a0f6ac1cb0c5ee0bc76434fe7010ca9
PxPixaro <akkirajagencies@gmail.com>
2026-06-28 13:10:13 +0530

    Add files via upload

---
+++ assets/matched_frames/manifest.js
@@ -0,0 +1,16 @@
+window.PIXARO_MATCHED_FRAMES = [
+  "assets/matched_frames/frame_001.jpg",
+  "assets/matched_frames/frame_002.jpg",
+  "assets/matched_frames/frame_003.jpg",
+  "assets/matched_frames/frame_004.jpg",
+  "assets/matched_frames/frame_005.jpg",
+  "assets/matched_frames/frame_006.jpg",
+  "assets/matched_frames/frame_007.jpg",
+  "assets/matched_frames/frame_008.jpg",
+  "assets/matched_frames/frame_009.jpg",
+  "assets/matched_frames/frame_010.jpg",
+  "assets/matched_frames/frame_011.jpg",
+  "assets/matched_frames/frame_012.jpg",
+  "assets/matched_frames/frame_013.jpg",
+  "assets/matched_frames/frame_014.jpg"
+];
